86249a89d2007d8a3f7c0b33b6587c7a5db9eda4
David Terry <dgterry@us.ibm.com>
2016-08-02 13:41:39 -0400

    remove use of contentText field (#35)
    - Update references for `contentText` to `body`

--- src/main/java/com/ibm/watson/apis/conversation_enhanced/utils/Constants.java
+++ src/main/java/com/ibm/watson/apis/conversation_enhanced/utils/Constants.java
@@ -18,7 +18,6 @@ public class Constants {
   // Schema Fields
   public static final String SCHEMA_FIELD_BODY = "body";
   public static final String SCHEMA_FIELD_CONTENT_HTML = "contentHtml";
-  public static final String SCHEMA_FIELD_CONTENT_TEXT = "contentText";
   public static final String SCHEMA_FIELD_ID = "id";
   public static final String SCHEMA_FIELD_SOURCE_URL = "sourceUrl";
   public static final String SCHEMA_FIELD_TITLE = "title";
